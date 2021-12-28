d557e4416dd823bb5d6ee6f98104e5d4325f9a54
yenbui6262 <yenbui6262@gmail.com>
2021-12-28 23:59:37 +0700

    file 2

---
+++ video72.js
@@ -0,0 +1,20 @@
+
+
+var box=document.querySelector('.box');
+
+console.log(box);
+
+//cach them element vao element da co
+
+box.innerHTML='<h1> new heading</h1>';
+
+//box.outerHTML='<span>heading</span>'; cách này nó  sẽ ghi đè luôn vào thẻ ban đầu
+
+
+
+//vẫn có thể lấy nội dung của h1 mới thêm
+console.log(document.querySelector('h1').innerText);
+
+console.log(box.innerHTML);
+
+console.log(box.outerHTML);//lấy luôn từ bên ngoài thẻ
